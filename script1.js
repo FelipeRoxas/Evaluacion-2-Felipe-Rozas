9e5f47b192461a7686f47d2f623baf407d63c317
FelipeRoxas <feliperozas5971@gmail.com>
2025-05-26 12:19:05 -0400

    Commit 2
    Segunda parte

--- script1.js
+++ script1.js
@@ -1,5 +1,42 @@
-function validar()
-let "nNombre"//validar nombre, debe contener solo letras y no ser vacio
-let//validar correo: Debe tener solo un @ y terminar en .cl
+let personas = []
+function validar (){
+    let nNombre = eNombre.value
+    let eNombre = document.getElementById("nombre")
+    let eErrorNombre = document.getElementById("errorNombre")
+    let nApellido = eApellido.value 
+    let eApellido = document.getElementById("Apellido") 
+    let eErrorApellido = document.getElementById("errorApellido")
+    let nPersona = ePersona.value 
+    let ePersona = document.getElementById("persona")
+    
+}
+let p = {
+    nombre:vNombre,
+    apellido:vApellido,
+}
 
-function 
+persona.push(p)
+console.log (persona)
+eNombre.value = ""
+eApellido.value = ""
+
+function eliminar (indice){
+    persona = persona.filter((p,index)=>{
+        if (index != indice){
+            return p
+        }
+    })
+    cargarDatos(),
+    console.log(persona)
+}
+function actualizarFormulario(indice){
+    let eApellido = getElementById("apellido")
+    let persona = personas.filter((p,index)=>{
+        if(index == indice){
+            return p
+        }
+    })
+}
+
+//validar nombre, debe contener solo letras y no ser vacio
+//validar correo: Debe tener solo un @ y terminar en .cl
